07dfa8234802e37dc89cd57fc7ddf962a4fd819b
Dilshard <mrdilshard@gmail.com>
2021-11-28 11:19:55 +0530

    Avg datatype changed, grade added.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,20 @@ public class Main {
         int mk2 = 88;
         int mk3 = 99;
         int total = mk1 + mk2 + mk3;
-        double avg = total / 3;
+        int avg = total / 3;
+        String grd;
+        if(avg>= 50){
+            grd = "Pass";
+        }else {
+            grd = "Fail";
+        }
+
 
         System.out.println("Marks 01 : "+mk1);
         System.out.println("Marks 02 : "+mk2);
         System.out.println("Marks 03 : "+mk3);
         System.out.println("Total : "+total);
         System.out.println("Average : "+avg);
+        System.out.println("Grade : "+grd);
     }
 }
