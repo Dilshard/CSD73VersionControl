5bebae01b57d5470987c7b73240f24b35cdc10d4
Dilshard <mrdilshard@gmail.com>
2021-11-28 11:10:20 +0530

    Initial commit, Student marks calculation!

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mk1 = 45;
+        int mk2 = 88;
+        int mk3 = 99;
+        int total = mk1 + mk2 + mk3;
+
+        System.out.println("Marks 01 : "+mk1);
+        System.out.println("Marks 02 : "+mk2);
+        System.out.println("Marks 03 : "+mk3);
+        System.out.println("Total : "+total);
+    }
+}
